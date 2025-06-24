3642bfdb56a0af9c7d3e7944acd2f3555a8c77b9
mmdgitraisoni <160129576+mmdgitraisoni@users.noreply.github.com>
2025-06-24 23:41:49 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MissionariesCannibalsGame from "./MissionariesCannibalsGame";
+
+function App() {
+  return (
+    <div>
+      <h1>Missionaries and Cannibals Puzzle Game</h1>
+      <MissionariesCannibalsGame />
+    </div>
+  );
+}
+
+export default App;
